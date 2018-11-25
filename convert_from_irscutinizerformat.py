78eef1ca68be64487ae07b1e41fc95608ad363e7
Peder Schmedling <24737456+Laffs2k5@users.noreply.github.com>
2018-11-25 22:03:03 +0100

    Add script to convert to broadlink format from file exported from IrScrutinizer (which accepts lirc format as input).

---
+++ convert_from_irscutinizerformat.py
@@ -0,0 +1,60 @@
+"""Helper script for convert irScrutinizer export to Openhab map format"""
+import argparse
+import binascii
+
+# Helper
+def add_zeros_for_broadlink_format(in_str):
+    # Multiples of 32 required by openhab broadlink add-on
+    # Add zeroes according to modulus of 32
+    mod_of_32 = (len(in_str) + 8) % 32
+    if mod_of_32 != 0:
+        missing_zeros_count = 32 - mod_of_32
+        #print 'was: ' + in_str  # debug
+        in_str += ('0' * missing_zeros_count)
+        #print ' is: ' + in_str  # debug
+    return in_str
+
+# "Main"
+def convert_from(irScutinizerFile,specific_ir_command=None,specific_ir_command_repetitons=None):
+    """Start convert ir codes to openhab map format"""
+
+    # import stuff
+    name = 'commands'
+    commands = getattr(__import__(irScutinizerFile, fromlist=[name]), name)
+
+    name = 'get_command_data'
+    get_command_data = getattr(__import__(irScutinizerFile, fromlist=[name]), name)
+
+    # print the stuff
+    if specific_ir_command is not None:
+        ir_payload = get_command_data(specific_ir_command, specific_ir_command_repetitons)
+        ir_payload_broadlink = add_zeros_for_broadlink_format(binascii.hexlify(ir_payload))
+        print irScutinizerFile + specific_ir_command.upper() + "=" + ir_payload_broadlink
+    else:
+        for ir_command, ir_data in commands.iteritems():
+            ir_payload = get_command_data(ir_command, 1)
+            ir_payload_broadlink = add_zeros_for_broadlink_format(binascii.hexlify(ir_payload))
+            print irScutinizerFile + ir_command.upper() + "=" + ir_payload_broadlink
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
+    parser.add_argument("--file", help="irScrutinizer-file to cinvert")
+    parser.add_argument("--command", help="Output payload for specific command only")
+    parser.add_argument("--count", default=1, type=int, help="Repetitions of ir connabd")
+    args = parser.parse_args()
+
+    if args.file:
+        # Sanitize
+        if args.file.startswith('.\\'):
+            args.file = args.file[2:]
+        if args.file.endswith('.py'):
+            args.file = args.file[:-3]
+        elif args.file.endswith('.pyc'):
+            args.file = args.file[:-4]
+
+        # Convert
+        if args.command:
+            convert_from(args.file, args.command, args.count)
+        else:
+            convert_from(args.file)
+
--- get_code.py
+++ get_code.py
@@ -4,7 +4,8 @@ import binascii
 import broadlink
 
 # config
-IP_OF_DEVICE = '192.168.0.119'
+IP_OF_DEVICE = '192.168.0.155'
+COMMAND_MAX_LEARNTIME_SEC=10
 
 # "Main"
 def start_capture():
@@ -16,17 +17,21 @@ def start_capture():
         device.auth()
 
         print "found. Learning from device:"
-        print "      ip:\t" + device.host[0]
-        print "    port:\t" + str(device.host[1])
+        print "             ip:  " + device.host[0]
+        print "           port:  " + str(device.host[1])
+        mac = binascii.hexlify(device.mac)[::-1]
+        print "  broadlink mac:  0x" + ''.join([mac[i:i+2] for i in range(0, len(mac), 2)])
         mac = binascii.hexlify(device.mac)
-        print "     mac:\t" + ':'.join([mac[i-2:i] for i in range(2, len(mac)+2, 2)][::-1])
-        print "     key:\t" + binascii.hexlify(device.key)
-        print "      iv:\t" + binascii.hexlify(device.iv)
+        print "        std mac:  " + ':'.join([mac[i-2:i] for i in range(2, len(mac)+2, 2)][::-1])
+        #print "     key:\t" + binascii.hexlify(device.key)
+        print "            key:  097628343fe99e23765c1513accf8b02"
+        #print "      iv:\t" + binascii.hexlify(device.iv)
+        print "             iv:  562e17996d093d28ddb3ba695a2e6f58"
 
         while True:
             device.enter_learning()
             print "\nSend IR now"
-            time.sleep(5)
+            time.sleep(COMMAND_MAX_LEARNTIME_SEC)
             ir_packet = []
             ir_packet = device.check_data()
             if ir_packet is not None:
